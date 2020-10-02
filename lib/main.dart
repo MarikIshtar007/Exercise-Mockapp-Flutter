bfb72c64bb8dfd449ea1f57fabd1b99d9244f985
Haany Ali <46514188+MarikIshtar007@users.noreply.github.com>
2020-10-02 15:35:39 +0530

    Testing the new SideBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,116 @@
 import 'package:flutter/material.dart';
-import 'screens/HomePage.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    ),
-  );
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: CustomAppBar(),
+        body: Container(color: Colors.yellow, alignment: Alignment.center));
+  }
+}
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(0.0, 1.0),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 4,
+            child: ClipPath(
+              clipper: AppBarClipper(),
+              clipBehavior: Clip.antiAlias,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(15),
+                      bottomRight: Radius.circular(25)),
+                  color: Colors.teal,
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                child: Text(
+                  'Ask a Student',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.notifications_active),
+                  color: Colors.grey,
+                  iconSize: 30,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.menu),
+                  color: Colors.teal,
+                  iconSize: 30,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(70.0);
+}
+
+class AppBarClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width * 0.85, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
 }
